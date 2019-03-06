e7571314eee3feb5cfc3be7b490c466887c12c0e
Louise Howard <howard.lou19@gmail.com>
2019-03-06 21:17:40 +0000

    Add files via upload

---
+++ CardCheck.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class CardCheck
+{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String num = new String("");
+		num = sc.next();
+		sc.close();
+
+		if(check(num))
+		{
+			System.out.println("Number is valid!");
+		}
+		else
+		{
+			System.out.println("Number is NOT valid!");
+		}
+	}
+
+
+    public static boolean check(String num)
+    {
+
+            int sum = 0;
+            boolean alternate = false;
+            for (int i = num.length() - 1; i >= 0; i--)
+            {
+                    int n = Integer.parseInt(num.substring(i, i + 1));
+                    if (alternate)
+                    {
+                            n *= 2;
+                            if (n > 9)
+                            {
+                                    n = (n % 10) + 1;
+                            }
+                    }
+                    sum += n;
+                    alternate = !alternate;
+            }
+            return (sum % 10 == 0);
+    }
+}
